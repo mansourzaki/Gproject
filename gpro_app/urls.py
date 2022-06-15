9a8ec47cdef8b638edad8737c09591342ce38e2a
Mansour <mansourzaki2000@gmail.com>
2022-06-16 01:59:45 +0300

    add routes

--- gpro_app/urls.py
+++ gpro_app/urls.py
@@ -2,6 +2,9 @@ import imp
 from django.urls import path
 from . import views 
 urlpatterns = [
-    path('',views.test,name='Test'),
+    path('subjects',views.subjects,name='subjects'),
+    path('',views.classes,name='classes'),
+    path('lecturers',views.lecturers,name='lecturers'),
+    path('yearly',views.yearlyStudents,name='yearlyStudents'),
 
 ]
--- gpro_app/views.py
+++ gpro_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def test(request):
+def subjects(request):
     return render(request,'pages/subjects.html')
+
+def classes(request):
+    return render(request,'pages/classes.html')
+
+def lecturers(request):
+    return render(request,'pages/lecturers.html')
+
+def yearlyStudents(request):
+    return render(request,'pages/yearly_students.html')
