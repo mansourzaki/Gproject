a2e0d490438be67096ba03008cb2c4130ef4646d
Mansour <mansourzaki2000@gmail.com>
2022-08-01 01:14:50 +0300

    all works

--- app.py
+++ app.py
@@ -12,30 +12,62 @@ CORS(app, support_credentials=True)
 def output_json():
     # with open('./iug_output1.json', 'r') as myfile:
     #  data = myfile.read()
-    with open('algo/classes/iug_output v6.json', 'r') as myfile:
+    with open('algo/classes/iug_output.json', 'r') as myfile:
         data = myfile.read()
     return data
 
-@app.route('/getinput',methods=['GET'])
+#get the first input
+@app.route('/getinput1',methods=['GET'])
 @cross_origin(origin='*')
-def getInput_json():
+def getInput_json1():
+
+    with open('algo/classes/iug_input1.json', 'r') as myfile:
+        data = myfile.read()
+    return data
+
+
+#get the second input
+@app.route('/getinput2',methods=['GET'])
+@cross_origin(origin='*')
+def getInput_json2():
     # with open('./iug_output1.json', 'r') as myfile:
     #  data = myfile.read()
-    with open('algo/classes/1st iug_input v4 grouping_modules.json', 'r') as myfile:
+    with open('algo/classes/iug_input2.json', 'r') as myfile:
         data = myfile.read()
     return data
 
-@app.route('/generateoutput',methods=['GET'])
+#run the the grouping and get the first output
+@app.route('/generatefirstoutput',methods=['GET'])
 @cross_origin(origin='*')
-def generateOutput_json():
+def generateFirstOutput_json():
+    subprocess.call(" python algo/groupingmodules.py ", shell=True)
+    return 'done'
+
+#run the initialsolution that produce the final output
+@app.route('/generatefinaloutput',methods=['GET'])
+@cross_origin(origin='*')
+def generateFinalOutputjson():
+    print('hii')
     subprocess.call(" python algo/first_solution.py ", shell=True)
     return 'done'
 
-
-@app.route('/input',methods=['POST'])
-def input_json():
+#uploads input2 file to algorithim
+@app.route('/loadinput2',methods=['POST'])
+def inputLoad2_json():
     request_data = request.data
     request_data = json.loads(request.data.decode('utf-8'))
+    with open('algo/classes/iug_input2.json', 'w') as write_file:
+       json.dump(request_data, write_file, indent=4)
+    return request_data
+
+#uploads input1 file to algorithim
+##check return invalid
+@app.route('/loadinput1',methods=['POST'])
+def inputLoad1_json():
+    request_data = request.data
+    request_data = json.loads(request.data.decode('utf-8'))
+    with open('algo/classes/iug_input1.json', 'w') as write_file:
+       json.dump(request_data, write_file, indent=4)
     return request_data
 
 if __name__ == '__main__':
